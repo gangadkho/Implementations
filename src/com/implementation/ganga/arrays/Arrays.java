62fc76d24d506c7bd9540e51621e8f661961b158
gangadharank <gangadharank@NCS-020217JJ05.ncs.corp.int-ads>
2017-05-26 17:59:23 +0800

    Added Arrays

---
+++ src/com/implementation/ganga/arrays/Arrays.java
@@ -0,0 +1,20 @@
+package com.implementation.ganga.arrays;
+
+public class Arrays {
+	int[] arr = new int[100];
+	public static void main(String arg[]) {
+		System.out.println("Implementation of Array");
+		
+	}
+	public void insert (int value) {
+		
+	}
+	public int find(int value) {
+		return 0;
+		
+	}
+	public void delete (int value) {
+		
+	}
+	
+}
